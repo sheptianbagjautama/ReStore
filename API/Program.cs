520e203e849597c57c488f3e6c559ca773b32eb7
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-09 16:04:48 +0700

    Add Packange EF Core
    Add Packange EF Core , using SQL Lite Database, and add Depedency Injenction into Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,13 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
