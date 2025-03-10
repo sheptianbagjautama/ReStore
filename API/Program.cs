3a1ca0bac76ea7c1c8effe37ce9772251c5b3cab
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-11 05:13:27 +0700

    Add Allow CORS

--- API/Program.cs
+++ API/Program.cs
@@ -4,14 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(); 
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 app.MapControllers();
 
