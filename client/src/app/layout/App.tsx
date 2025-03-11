b5aed28cd86cbb2e178a9bd6e821e16776314dc1
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-12 05:35:07 +0700

    Add Darkmode feature

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,10 @@
-import { Container } from "@mui/material";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import Catalog from "../features/catalog/Catalog";
 import { Product } from "../models/product";
@@ -6,6 +12,22 @@ import NavBar from "./NavBar";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState<boolean>(true)
+
+  const palleteType = darkMode ? "dark" : "light";
+
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: palleteType === "light" ? "#121212" : "#eaeaea",
+      },
+    },
+  });
+
+  const toogleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -14,12 +36,20 @@ function App() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Container maxWidth="xl" sx={{mt:14}}>
-        <Catalog products={products} />
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar darkMode={darkMode} toogleDarkMode={toogleDarkMode}/>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: darkMode ? "#121212" : "#eaeaea",
+        }}
+      >
+        <Container maxWidth="xl" sx={{ pt: 14 }}>
+          <Catalog products={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,10 +1,21 @@
-import {AppBar, Toolbar, Typography} from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
 
-export default function NavBar() {
+type Props = {
+  darkMode:boolean,
+  toogleDarkMode:() => void
+}
+
+export default function NavBar({darkMode,toogleDarkMode}:Props) {
+  // const darkMode = false;
+
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6">RE-STORE</Typography>
+        <IconButton onClick={toogleDarkMode}>
+          {darkMode ? <DarkMode/> : <LightMode sx={{color:'yellow'}}/>}
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
