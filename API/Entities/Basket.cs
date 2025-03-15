69a5765ed81eaf91762c8eaf53c430f78e322e0a
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-16 05:19:18 +0700

    Adding Basket, BasketItem entity

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public required string BasketId { get; set; }
+        public List<BasketItem> Items { get; set; } = [];
+    }
+}
---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Entities
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        //navigation properties
+        public int ProductId { get; set; }
+        public required Product Product { get; set; }
+    }
+}
