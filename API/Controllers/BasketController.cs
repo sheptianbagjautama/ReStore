de336503d1af1fecde4bb686487221071e4a08ee
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-18 00:55:49 +0700

    Create BasketController.cs

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController(StoreContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await context.Baskets
+                .Include(x => x.Items)
+                .ThenInclude(x => x.Product)
+                .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+
+            if(basket == null) return NoContent();
+
+            return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            // get basket
+            // create basket
+            // get product
+            // add item to basket
+            // save changes
+
+            return StatusCode(201);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+            //get basket
+            //remove the item or reduce its quantity
+            //save changes
+            return Ok();
+        }
+    }
+}
