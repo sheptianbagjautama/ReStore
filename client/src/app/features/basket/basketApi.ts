bfbbb20b06bf064c8d41e3e4f356d26ea6b51519
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-18 21:28:41 +0700

    Invalidating RTK Query cache

--- client/src/app/features/basket/basketApi.ts
+++ client/src/app/features/basket/basketApi.ts
@@ -5,15 +5,25 @@ import { Basket } from "../../models/basket";
 export const basketApi = createApi({
   reducerPath: "basketApi",
   baseQuery: baseQueryWithErrorHandling,
+  tagTypes:['Basket'],
   endpoints: (builder) => ({
     fetchBasket: builder.query<Basket, void>({
       query: () => "basket",
+      providesTags:['Basket']
     }),
     addBasketItem: builder.mutation<Basket,{ productId: number; quantity: number}>({
       query: ({ productId, quantity }) => ({
         url: `basket?productId=${productId}&quantity=${quantity}`,
         method: "POST",
       }),
+      onQueryStarted:async (_, {dispatch, queryFulfilled}) => {
+        try {
+          await queryFulfilled;
+          dispatch(basketApi.util.invalidateTags(['Basket']));
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }),
     removeBasketItem: builder.mutation<Basket,{ productId: number; quantity: number }>({
       query: ({ productId, quantity }) => ({
