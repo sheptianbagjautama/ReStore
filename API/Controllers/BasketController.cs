e80e470eafdf17b981cdd0a9da81c47650a09d83
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-18 11:34:28 +0700

    Using extension methods in C# for reusability

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Data;
 using API.Dtos;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,24 +17,11 @@ namespace API.Controllers
 
             if (basket == null) return NoContent();
 
-            return new BasketDto
-            {
-                BasketId = basket.BasketId,
-                Items = basket.Items.Select(x => new BasketItemDto
-                {
-                    ProductId = x.ProductId,
-                    Name = x.Product.Name,
-                    Price = x.Product.Price,
-                    Brand = x.Product.Brand,
-                    Type = x.Product.Type, 
-                    PictureUrl = x.Product.PictureUrl, 
-                    Quantity = x.Quantity
-                }).ToList()
-            };
+            return basket.ToDto();
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
         {
             var basket = await RetrieveBasket();
 
@@ -47,7 +35,7 @@ namespace API.Controllers
 
             var result = await context.SaveChangesAsync() > 0;
 
-            if (result) return CreatedAtAction(nameof(GetBasket), basket);
+            if (result) return CreatedAtAction(nameof(GetBasket), basket.ToDto());
 
             return BadRequest("Problem updating basket");
         }
---
+++ API/Extensions/BasketExtentions.cs
@@ -0,0 +1,28 @@
+﻿
+
+using API.Dtos;
+using API.Entities;
+
+namespace API.Extensions
+{
+    public static class BasketExtentions
+    {
+        public static BasketDto ToDto(this Basket basket)
+        {
+            return new BasketDto
+            {
+                BasketId = basket.BasketId,
+                Items = basket.Items.Select(x => new BasketItemDto
+                {
+                    ProductId = x.ProductId,
+                    Name = x.Product.Name,
+                    Price = x.Product.Price,
+                    Brand = x.Product.Brand,
+                    Type = x.Product.Type,
+                    PictureUrl = x.Product.PictureUrl,
+                    Quantity = x.Quantity
+                }).ToList()
+            };
+        }
+    }
+}
