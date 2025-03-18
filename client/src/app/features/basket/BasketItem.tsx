9dc1d300480722d000abd3626a03a10361a833e6
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-18 21:16:56 +0700

    Styling the basket

---
+++ client/src/app/features/basket/BasketItem.tsx
@@ -0,0 +1,78 @@
+import { Box, Grid2, IconButton, Paper, Typography } from "@mui/material";
+import { Item } from "../../models/basket";
+import { Add, Close, Remove } from "@mui/icons-material";
+
+type Props = {
+  item: Item;
+};
+
+export default function BasketItem({ item }: Props) {
+  return (
+    <Paper
+      sx={{
+        height: 140,
+        borderRadius: 3,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mb: 2,
+      }}
+    >
+      <Box display="flex" alignItems="center">
+        <Box
+          component="img"
+          src={item.pictureUrl}
+          alt={item.name}
+          sx={{
+            width: 100,
+            height: 100,
+            objectFit: "cover",
+            borderRadius: "4px",
+            mr: 8,
+            ml: 4,
+          }}
+        />
+
+        <Box display="flex" flexDirection="column" gap={1}>
+          <Typography variant="h6">{item.name}</Typography>
+
+          <Box display="flex" alignItems="center" gap={3}>
+            <Typography sx={{ fontSize: "1.1rem" }}>
+              ${(item.price / 100).toFixed(2)} x {item.quantity}
+            </Typography>
+
+            <Typography sx={{ fontSize: "1.1rem" }} color="primary">
+              ${((item.price / 100) * item.quantity).toFixed()}
+            </Typography>
+          </Box>
+
+          <Grid2 container spacing={1} alignItems="center">
+            <IconButton
+              color="error"
+              size="small"
+              sx={{ border: 1, borderRadius: 1, minWidth: 0 }}
+            >
+              <Remove />
+            </IconButton>
+            <Typography variant="h6">{item.quantity}</Typography>
+            <IconButton
+              color="success"
+              size="small"
+              sx={{ border: 1, borderRadius: 1, minWidth: 0 }}
+            >
+              <Add />
+            </IconButton>
+          </Grid2>
+        </Box>
+      </Box>
+
+      <IconButton
+        color="error"
+        size="small"
+        sx={{ border: 1, borderRadius: 1, minWidth: 0, alignSelf: "start", mt:1, mr:1 }}
+      >
+        <Close />
+      </IconButton>
+    </Paper>
+  );
+}
--- client/src/app/features/basket/BasketPage.tsx
+++ client/src/app/features/basket/BasketPage.tsx
@@ -1,5 +1,6 @@
-import { Typography } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { useFetchBasketQuery } from "./basketApi"
+import BasketItem from "./BasketItem";
 
 export default function BasketPage() {
     const {data, isLoading} = useFetchBasketQuery();
@@ -9,6 +10,12 @@ export default function BasketPage() {
     if(!data) return <Typography variant="h3">Your basket is empty</Typography>
 
   return (
-    <div>{data.basketId}</div>
+    <Grid2 container spacing={2}>
+      <Grid2 size={8}>
+        {data.items.map(item => (
+          <BasketItem item={item} key={item.productId}/>
+        ))}
+      </Grid2>
+    </Grid2>
   )
 }
