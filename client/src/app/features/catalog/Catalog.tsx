f8b11a5f53f1428eb157ed58b565e3d388b84108
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-23 02:41:44 +0700

    Setting up pagination on the client part 2

--- client/src/app/features/catalog/Catalog.tsx
+++ client/src/app/features/catalog/Catalog.tsx
@@ -1,8 +1,10 @@
-import { Grid2, Pagination } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
+import AppPagination from "../../shared/components/AppPagination";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import { useFetchProductsQuery } from "./catalogApi";
-import ProductList from "./ProductList";
+import { setPageNumber } from "./catalogSlice";
 import Filters from "./Filters";
-import { useAppSelector } from "../../store/store";
+import ProductList from "./ProductList";
 
 export default function Catalog() {
   ////Cara menggunakan useEffect
@@ -17,6 +19,7 @@ export default function Catalog() {
   ////Cara menggunakan RTK Query
   const productParams = useAppSelector((state) => state.catalog);
   const { data, isLoading } = useFetchProductsQuery(productParams);
+  const dispatch = useAppDispatch();
 
   if (isLoading || !data) return <div>Loading...</div>;
 
@@ -26,13 +29,19 @@ export default function Catalog() {
         <Filters />
       </Grid2>
       <Grid2 size={9}>
-        <ProductList products={data.items} />
-        <Pagination
-          color="secondary"
-          size="large"
-          count={data.pagination.totalPages}
-          page={data.pagination.currentPage}
-        />
+        {data.items && data.items.length > 0 ? (
+          <>
+            <ProductList products={data.items} />
+            <AppPagination
+              metadata={data.pagination}
+              onPageChange={(page: number) => dispatch(setPageNumber(page))}
+            />
+          </>
+        ) : (
+          <Typography variant="h5">
+            There are no results to this filter
+          </Typography>
+        )}
       </Grid2>
     </Grid2>
   );
