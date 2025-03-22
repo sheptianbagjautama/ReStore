9c8762623826cc08e697ca0c6e516937b51ef143
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-22 15:26:27 +0700

    Designing the filters component

--- client/src/app/features/catalog/Filters.tsx
+++ client/src/app/features/catalog/Filters.tsx
@@ -1,11 +1,71 @@
-import { useFetchFiltersQuery } from "./catalogApi"
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Radio,
+  TextField,
+} from "@mui/material";
+import { useFetchFiltersQuery } from "./catalogApi";
+import { CheckBox } from "@mui/icons-material";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price: High to low" },
+  { value: "price", label: "Price: Low to hight" },
+];
 
 export default function Filters() {
-    const {data} = useFetchFiltersQuery();
+  const { data } = useFetchFiltersQuery();
 
-    console.log(data);
-    
+  console.log(data);
+
   return (
-    <div>Filters</div>
-  )
-} 
+    <Box display="flex" flexDirection="column" gap={3}>
+      <Paper>
+        <TextField label="Search products" variant="outlined" fullWidth />
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormControl>
+          {sortOptions.map(({ value, label }) => (
+            <FormControlLabel
+              key="label"
+              control={<Radio sx={{ px: 0.7 }} />}
+              label={label}
+              value={value}
+            />
+          ))}
+        </FormControl>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.brands.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <CheckBox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.types.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <CheckBox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+    </Box>
+  );
+}
