0eb1c61465e01e503f0dfc86699a8a5a556c67de
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-28 17:38:50 +0700

    Adding a user menu

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -14,6 +14,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { setDarkMode } from "./uiSlice";
 import { useFetchBasketQuery } from "../features/basket/basketApi";
+import UserMenu from "./UserMenu";
 
 const midLinks = [
   { title: "catalog", path: "/catalog" },
@@ -39,16 +40,18 @@ const navStyles = {
 };
 
 export default function NavBar() {
-  const {isLoading,darkMode} = useAppSelector(state => state.ui);
+  const user = { email: "test@test.com", roles:[] };
+  const { isLoading, darkMode } = useAppSelector((state) => state.ui);
   const dispatch = useAppDispatch();
-  const {data:basket} = useFetchBasketQuery();
+  const { data: basket } = useFetchBasketQuery();
 
-  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
+  const itemCount =
+    basket?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   return (
     <AppBar position="fixed">
-      <Toolbar sx={{justifyContent:'space-between', alignItems:'center'}}>
-        <Box display='flex' alignItems='center'>
+      <Toolbar sx={{ justifyContent: "space-between", alignItems: "center" }}>
+        <Box display="flex" alignItems="center">
           <Typography component={NavLink} sx={navStyles} to="/" variant="h6">
             RE-STORE
           </Typography>
@@ -65,25 +68,39 @@ export default function NavBar() {
           ))}
         </List>
 
-        <Box display='flex' alignItems='center'>
-          <IconButton component={Link} to="/basket" size="large" sx={{ color: "inherit" }}>
+        <Box display="flex" alignItems="center">
+          <IconButton
+            component={Link}
+            to="/basket"
+            size="large"
+            sx={{ color: "inherit" }}
+          >
             <Badge badgeContent={itemCount} color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
 
-          <List sx={{ display: "flex" }}>
-            {rightLinks.map(({ title, path }) => (
-              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+          {user ? (
+            <UserMenu user={user} />
+          ) : (
+            <List sx={{ display: "flex" }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
       </Toolbar>
       {isLoading && (
-        <Box sx={{width:'100%'}}>
-          <LinearProgress color="secondary"/>
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress color="secondary" />
         </Box>
       )}
     </AppBar>
