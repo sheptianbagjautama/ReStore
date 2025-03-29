55d03975752c070177c0094c8c60189a187738d5
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-03-30 00:07:43 +0700

    Handling server side errors

--- client/src/app/features/account/accountApi.ts
+++ client/src/app/features/account/accountApi.ts
@@ -42,6 +42,7 @@ export const accountApi = createApi({
           router.navigate('/login');
         } catch (error) {
           console.log(error)
+          throw error
         }
       }
     }),
--- client/src/lib/schemas/registerSchema.ts
+++ client/src/lib/schemas/registerSchema.ts
@@ -8,7 +8,7 @@ export const registerSchema = z.object({
     email:z.string().email(),
     password:z.string().regex(passwordValidation, {
         message:'Password must contain 1 lowercase alpha, 1 uppercase character, 1 number, 1 special and 6-10 characters '
-    })
+    }),
 });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
