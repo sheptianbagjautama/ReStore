63175e2273f1a5bcaa5a5b4ba9b7137e5409ac6a
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-04-04 04:41:45 +0700

    Adding the address element

--- client/src/app/features/checkout/CheckoutPage.tsx
+++ client/src/app/features/checkout/CheckoutPage.tsx
@@ -6,33 +6,39 @@ import { useEffect, useMemo, useRef } from "react";
 import { useFetchBasketQuery } from "../basket/basketApi";
 import { Elements } from "@stripe/react-stripe-js";
 import { useCreatePaymentIntentMutation } from "./checkoutApi";
+import { useAppSelector } from "../../store/store";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 
 export default function CheckoutPage() {
   const { data: basket } = useFetchBasketQuery();
-  const [createPaymentIntent, {isLoading}] = useCreatePaymentIntentMutation();
-  
+  const [createPaymentIntent, { isLoading }] = useCreatePaymentIntentMutation();
+
   /**
    * Digunakan untuk keperluan di development agar tidak load 2 kali ketika create payment intent stripe
    * karena di development react menggunakan strict yang dimana strict ini memproses seperti useEffect load 2 kali
    */
-  const created = useRef(false); 
+  const created = useRef(false);
+  const { darkMode } = useAppSelector((state) => state.ui);
 
   useEffect(() => {
     //hanya di gunakan di development , di production code ini tidak digunakan
-    if(!created.current) createPaymentIntent(); //jika false maka akan update basket ke stripe 
+    if (!created.current) createPaymentIntent(); //jika false maka akan update basket ke stripe
     created.current = true; //jika true makan tidak akan update basket ke stripe
-  }, [createPaymentIntent])
-  
+  }, [createPaymentIntent]);
 
   const options: StripeElementsOptions | undefined = useMemo(() => {
     if (!basket?.clientSecret) return undefined;
 
     return {
       clientSecret: basket.clientSecret,
+      //mengatur input ketika dark mode
+      appearance: {
+        labels: "floating",
+        theme: darkMode ? "night" : "stripe",
+      },
     };
-  }, [basket?.clientSecret]);
+  }, [basket?.clientSecret, darkMode]);
 
   return (
     <Grid2 container spacing={2}>
@@ -40,7 +46,7 @@ export default function CheckoutPage() {
         {!stripePromise || !options || isLoading ? (
           <Typography variant="h6">Loading checkout...</Typography>
         ) : (
-          <Elements stripe={stripePromise} options={options }>
+          <Elements stripe={stripePromise} options={options}>
             <CheckoutStepper />
           </Elements>
         )}
--- client/src/app/features/checkout/CheckoutStepper.tsx
+++ client/src/app/features/checkout/CheckoutStepper.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Paper, Step, StepLabel, Stepper } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+} from "@mui/material";
+import { AddressElement } from "@stripe/react-stripe-js";
 import { useState } from "react";
 
 const steps = ["Address", "Payment", "Review"];
@@ -26,19 +36,24 @@ export default function CheckoutStepper() {
         })}
       </Stepper>
 
-      <Box sx={{mt:2}}>
-        <Box sx={{display:activeStep === 0 ? 'block' : 'none' }}>
-            Address step
+      <Box sx={{ mt: 2 }}>
+        <Box sx={{ display: activeStep === 0 ? "block" : "none" }}>
+          <AddressElement options={{ mode: "shipping" }} />
+          <FormControlLabel
+            sx={{ display: "flex", justifyContent: "end" }}
+            control={<Checkbox />}
+            label="Save as default address"
+          />
         </Box>
-        <Box sx={{display:activeStep === 1 ? 'block' : 'none' }}>
-            Payment step
+        <Box sx={{ display: activeStep === 1 ? "block" : "none" }}>
+          Payment step
         </Box>
-        <Box sx={{display:activeStep === 2 ? 'block' : 'none' }}>
-            Review step
+        <Box sx={{ display: activeStep === 2 ? "block" : "none" }}>
+          Review step
         </Box>
       </Box>
 
-      <Box display='flex' paddingTop={2} justifyContent='space-between'>
+      <Box display="flex" paddingTop={2} justifyContent="space-between">
         <Button onClick={handleBack}>Back</Button>
         <Button onClick={handleNext}>Next</Button>
       </Box>
