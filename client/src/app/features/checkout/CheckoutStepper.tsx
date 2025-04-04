0519e1da5ed3fa9de0876970fa8ca285f6ff6dea
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-04-04 19:25:56 +0700

    14. Update address functionality

--- client/src/app/features/checkout/CheckoutStepper.tsx
+++ client/src/app/features/checkout/CheckoutStepper.tsx
@@ -7,29 +7,54 @@ import {
   Step,
   StepLabel,
   Stepper,
+  Typography,
 } from "@mui/material";
-import { AddressElement, PaymentElement } from "@stripe/react-stripe-js";
+import { AddressElement, PaymentElement, useElements } from "@stripe/react-stripe-js";
 import { useState } from "react";
 import Review from "./Review";
-import { useFetchAddressQuery } from "../account/accountApi";
+import { useFetchAddressQuery, useUpdateUserAddressMutation } from "../account/accountApi";
 import { Address } from "../../models/user";
 
 const steps = ["Address", "Payment", "Review"];
 
 export default function CheckoutStepper() {
   const [activeStep, setActiveStep] = useState(0);
-  const { data: { name, ...restAddress } = {} as Address } =
-    useFetchAddressQuery();
+  const { data: { name, ...restAddress } = {} as Address, isLoading } = useFetchAddressQuery();
+  const [updateAddress] = useUpdateUserAddressMutation();
+  const [saveAddressChecked, setSaveAddressChecked] = useState(false);
+  const elements = useElements();
 
-    console.log(name, restAddress);
-
-  const handleNext = () => {
+  const handleNext = async () => {
+    if(activeStep === 0 && saveAddressChecked && elements) {
+      const address = await getStripeAddress();
+      if(address) await updateAddress(address);
+    } 
     setActiveStep((step) => step + 1);
   };
+
+  const getStripeAddress = async () => {
+    // Mengambil elemen 'address' dari objek 'elements', yang kemungkinan merupakan bagian dari library Stripe.
+    const addressElement = elements?.getElement('address');
+
+    // Jika elemen 'address' tidak ditemukan, fungsi akan langsung mengembalikan null.
+    if (!addressElement) return null;
+
+    // Mengambil nilai 'name' dan 'address' dari 'addressElement'.
+    const { value: { name, address } } = await addressElement.getValue();
+
+    // Jika 'name' dan 'address' ada (tidak undefined atau null), mengembalikan objek yang berisi properti 'name' dan 'address'.
+    // Ini akan berguna untuk mendapatkan informasi alamat lengkap untuk diproses lebih lanjut.
+    if (name && address) return { ...address, name };
+
+    // Jika tidak ada 'name' atau 'address', fungsi mengembalikan null.
+    return null;
+}
 
   const handleBack = () => {
     setActiveStep((step) => step - 1);
   };
+
+  if(isLoading) return <Typography variant="h6">Loading checkout...</Typography>
 
   return (
     <Paper sx={{ p: 3, borderRadius: 3 }}>
@@ -56,7 +81,9 @@ export default function CheckoutStepper() {
           />
           <FormControlLabel
             sx={{ display: "flex", justifyContent: "end" }}
-            control={<Checkbox />}
+            control={<Checkbox 
+            checked={saveAddressChecked}
+            onChange={e => setSaveAddressChecked(e.target.checked)}/>}
             label="Save as default address"
           />
         </Box>
