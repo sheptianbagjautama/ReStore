adcf95bac95b3100bb8658118340b766085a3633
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-04-05 23:32:00 +0700

    ORDER FEATURE - Creating the order entities part 2

--- API/Data/StoreContex.cs
+++ API/Data/StoreContex.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ namespace API.Data;
 public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options) { 
     public required DbSet<Product> Products { get; set; }
     public required DbSet<Basket> Baskets { get; set; }
+    public required DbSet<Order> Orders { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
