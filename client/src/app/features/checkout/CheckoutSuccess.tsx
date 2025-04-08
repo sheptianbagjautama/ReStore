b76eae417fddea898f725f578d4c78d278b40c01
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-04-08 23:05:24 +0700

    ORDER FEATURE - 11. Adding a checkout success page

--- client/src/app/features/checkout/CheckoutSuccess.tsx
+++ client/src/app/features/checkout/CheckoutSuccess.tsx
@@ -1,14 +1,111 @@
-import { Typography } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 import { Order } from "../../models/order";
+import { currencyFormat } from "../../../lib/util";
 
 export default function CheckoutSuccess() {
-  const {state} = useLocation();
-  const order = state as Order;
+  const { state } = useLocation();
+  const order = state.data as Order;
+
+  if (!order) return <Typography>Problem accessing the order</Typography>;
+
+  const addressString = () => {
+    const address = order.shippingAddress;
+    return `${address?.name}, ${address?.line1}, ${address?.city}, ${address?.state}, ${address?.postal_code}, ${address?.country}`;
+  };
+
+  const paymentString = () => {
+    const card = order.paymentSummary;
+    return `${card?.brand.toUpperCase()}, **** **** **** ${card?.last4}, Exp: ${
+      card?.exp_month
+    }/${card?.exp_year}`;
+  };
 
   return (
-    <Typography variant="h5">
-        {JSON.stringify(order, null, 2)}
-    </Typography>
-  )
+    <Container maxWidth="md">
+      <>
+        <Typography variant="h4" gutterBottom fontWeight="bold">
+          Thanks for your fake order!
+        </Typography>
+        <Typography variant="body1" color="textSecondary" gutterBottom>
+          Your order <strong>#{order.id}</strong> will never be processed as
+          this is fake shop.
+        </Typography>
+
+        <Paper
+          elevation={1}
+          sx={{
+            p: 2,
+            mb: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1.5,
+          }}
+        >
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="body2" color="textSecondary">
+              Order date
+            </Typography>
+            <Typography variant="body2" fontWeight="bold">
+              {order.orderDate}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="body2" color="textSecondary">
+              Payment method
+            </Typography>
+            <Typography variant="body2" fontWeight="bold">
+              {paymentString()}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="body2" color="textSecondary">
+              Shipping address
+            </Typography>
+            <Typography variant="body2" fontWeight="bold">
+              {addressString()}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="body2" color="textSecondary">
+              Amount
+            </Typography>
+            <Typography variant="body2" fontWeight="bold">
+              {currencyFormat(order.total)}
+            </Typography>
+          </Box>
+        </Paper>
+
+        <Box display="flex" justifyContent="flex-start" gap={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            to={`/orders/${order.id}`}
+          >
+            View your order
+          </Button>
+
+          <Button
+            component={Link}
+            to="/catalog"
+            variant="outlined"
+            color="primary"
+          >
+            Continue shopping
+          </Button>
+        </Box>
+      </>
+    </Container>
+  );
 }
