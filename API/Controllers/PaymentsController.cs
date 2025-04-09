c0721bf7c4e4f3bbbebfd01551e18c137fcc5292
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-04-10 06:34:12 +0700

    ORDER FEATURE - 15. Using Webhooks with Stripe part 1

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -5,6 +5,7 @@ using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -12,11 +13,15 @@ namespace API.Controllers
     {
         private readonly PaymentsService paymentsService;
         private readonly StoreContext storeContext;
+        private readonly IConfiguration configuration;
+        private readonly ILogger<PaymentsController> logger;
 
-        public PaymentsController(PaymentsService paymentsService, StoreContext storeContext)
+        public PaymentsController(PaymentsService paymentsService, StoreContext storeContext, IConfiguration configuration, ILogger<PaymentsController> logger)
         {
             this.paymentsService = paymentsService;
             this.storeContext = storeContext;
+            this.configuration = configuration;
+            this.logger = logger;
         }
 
         [Authorize]
@@ -45,6 +50,56 @@ namespace API.Controllers
             return basket.ToDto();
         }
 
+        [HttpPost("webhook")]
+        public async Task<IActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(Request.Body).ReadToEndAsync(); 
 
+            try
+            {
+                var stripeEvent = ConstructStripeEvent(json);
+
+                if(stripeEvent.Data.Object is not PaymentIntent intent) {
+                    return BadRequest("Invalid event data");
+                }
+
+                if(intent.Status == "succeeded") await HandlePaymentIntentSucceeded(intent);
+                else await HandlePaymentIntentFailed(intent);
+
+                return Ok();
+            }
+            catch (StripeException ex)
+            {
+                logger.LogError(ex, "Stripe webhook error");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Webhook error");
+            }
+            catch(Exception ex) {
+                logger.LogError(ex, "An Unexpected error has occurred");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unexpected error");
+            }
+        }
+
+        private async Task HandlePaymentIntentFailed(PaymentIntent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandlePaymentIntentSucceeded(PaymentIntent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Event ConstructStripeEvent(string json)
+        {
+            try
+            {
+                return EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], configuration["StripeSettings:WhSecret"]);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to construct stripe event");
+                throw new StripeException("Invalid signature");
+            }
+        }
     }
 }
